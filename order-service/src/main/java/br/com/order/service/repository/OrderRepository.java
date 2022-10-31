7f2d4b1a77354b978aa36d9c5bb4c9052a805ec4
Ronaldo Melo <rpm.melkor@gmail.com>
2022-10-31 01:14:59 -0300

    feat: order repository

---
+++ order-service/src/main/java/br/com/order/service/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package br.com.order.service.repository;
+
+import br.com.order.service.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
